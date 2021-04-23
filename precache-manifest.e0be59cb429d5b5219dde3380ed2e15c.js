179aab3cfcd11f6bf536f0fb6b858dbe67a0381f
Aviral Tandon <aviraltandon20@gmail.com>
2021-04-23 13:32:25 +0530

    Updates

---
+++ precache-manifest.e0be59cb429d5b5219dde3380ed2e15c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f18c52ac815cbd3a1713fce22567fd93",
+    "url": "/Movie-Database/index.html"
+  },
+  {
+    "revision": "08ddf2b3586648887154",
+    "url": "/Movie-Database/static/css/main.7b2072b9.chunk.css"
+  },
+  {
+    "revision": "422784743bc642a06481",
+    "url": "/Movie-Database/static/js/2.cd1e6303.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Movie-Database/static/js/2.cd1e6303.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08ddf2b3586648887154",
+    "url": "/Movie-Database/static/js/main.b0e45185.chunk.js"
+  },
+  {
+    "revision": "59ffac50953d9bbe4bdb",
+    "url": "/Movie-Database/static/js/runtime-main.5283d801.js"
+  }
+]);
